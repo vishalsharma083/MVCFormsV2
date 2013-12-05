2e7f581ea9852ff841f34a834c3b7e70ee4e8243
vishal <vishal@fareportal.com>
2013-12-05 20:51:56 +0530

    form saving in db

---
+++ mvcdynamicforms_ef8fb2ed1afb/MvcDynamicForms.Core/Utility.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+
+namespace MVCDynamicForms
+{
+    public class Utility
+    {
+        public static string GetFormFieldValue(NameValueCollection Collection, string FieldName)
+        {
+            if (Collection[FieldName] != null)
+            {
+                return Collection[FieldName].Trim();
+            }
+            return string.Empty;
+        }
+    }
+}
